95f27305a9d918d66959c044b3f0befca0a96c4e
Jessica Terry <blackblue58@hotmail.com>
2025-07-06 18:56:30 -0700

    fixed message route

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,12 +1,3 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-/* GET all messages */
-router.get('/', function(req, res, next) {
-  res.status(200).json({
-    message: 'Messages fetched successfully!',
-    messages: [] // Placeholder for actual messages
-  });
-});
-
 module.exports = router;
